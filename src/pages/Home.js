ceb42558e439272e95c161d24c6d34a1dc8abef1
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-05 07:36:54 +0500

    Add initial project files and structure

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import { Box } from "@mui/material";
+import React, { useState } from "react";
+import Exercises from "../components/Exercises";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercise from "../components/SearchExercise";
+const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
+  return (
+    <Box>
+      <HeroBanner />
+
+      <SearchExercise
+        setExercises={setExercises}
+        setBodyPart={setBodyPart}
+        bodyPart={bodyPart}
+      />
+
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        exercises={exercises}
+      />
+    </Box>
+  );
+};
+
+export default Home;
